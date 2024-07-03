0b848a67a7d80c896eb9c0fdb202edf850dc4657
aditya <adityachauhan053@gmail.com>
2024-07-03 21:58:06 +0530

    chart color

--- src/Chart/BarChart.jsx
+++ src/Chart/BarChart.jsx
@@ -7,6 +7,13 @@ export const data = [
   ["2015", 1170, 460, 250],
   ["2016", 660, 1120, 300],
   ["2017", 1030, 540, 350],
+  ["2018", 1000, 400, 200],
+  ["2019", 1170, 460, 250],
+  ["2020", 660, 1120, 300],
+  ["2021", 1030, 540, 350],
+  ["2022", 660, 1120, 300],
+  ["2023", 1030, 540, 350],
+  ["2024", 1030, 540, 350],
 ];
 
 export const options = {
@@ -14,6 +21,7 @@ export const options = {
     title: "Company Performance",
     subtitle: "Sales, Expenses, and Profit: 2014-2017",
   },
+  colors:['rgb(85, 61, 222)','rgb(38, 183, 152)','rgb(93, 104, 170)']
 };
 
 export default function BarChart() {
@@ -21,7 +29,7 @@ export default function BarChart() {
     <Chart
       chartType="Bar"
       width="100%"
-      height="400px"
+      height="600px"
       data={data}
       options={options}
     />
